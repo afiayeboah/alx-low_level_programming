a71d78e5055c9ba37a3471ed1c4c10d55d3d05fe
afiayeboah <afia.yakuamoah@gmail.com>
2023-08-17 18:18:44 -0700

    corrected 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,16 @@
 
 void print_number(int n)
 {
-unsigned int num = n;
+unsigned int a = n;
 if (n < 0)
 {
-_putchar (' ');
-num = -num;
+_putchar (45);
+a = -a;
 }
-if ((num / 10) > 0)
-print_number(num / 10);
-_putchar(num % 10 + '0');
+if ((a / 10) > 0)
+{
+
+print_number(a / 10);
+}
+_putchar(a % 10 + '0');
 }
