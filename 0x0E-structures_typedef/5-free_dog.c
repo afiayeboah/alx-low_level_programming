20df5e5ed83350ea717594cd1f782f799068a0cb
afiayeboah <afia.yakuamoah@gmail.com>
2023-09-11 06:31:01 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+ * free_dog - function that frees memory allocated for dog
+ * @d: the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
