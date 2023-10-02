1eabb14974869c512c376bf2e3c71c96b6754368
afiayeboah <afia.yakuamoah@gmail.com>
2023-10-02 15:38:22 -0700

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,106 +1,94 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUF_SIZE 1024
 
 /**
- * create_buffer - Allocates memory for a buffer.
- *
- * Return: A pointer to the newly-allocated buffer, or NULL on failure.
+ * print_usage_error - Print usage error message and exit with code 97.
  */
-char *create_buffer(void)
+void print_usage_error(void)
 {
-    char *buffer = malloc(BUF_SIZE);
-
-    if (buffer == NULL)
-    {
-        perror("Error: Unable to allocate buffer");
-        exit(99);
-    }
-
-    return (buffer);
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 }
 
 /**
- * close_file - Closes a file descriptor.
- * @fd: The file descriptor to be closed.
+ * print_read_error - Print read error message and exit with code 98.
+ * @filename: Name of the file causing the error.
  */
-void close_file(int fd)
+void print_read_error(const char *filename)
 {
-    if (close(fd) == -1)
-    {
-        perror("Error: Unable to close file descriptor");
-        exit(100);
-    }
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
 }
 
 /**
- * main - Copies the contents of one file to another.
+ * print_write_error - Print write error message and exit with code 99.
+ * @filename: Name of the file causing the error.
+ */
+void print_write_error(const char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+/**
+ * print_close_error - Print file descriptor close
+ * error message and exit with code 100.
+ * @fd: File descriptor causing the error.
+ * main - Copies content of a fie to another file
+ */
+void print_close_error(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * main - Entry point for copying the contents of one file to another.
  * @argc: The number of arguments provided to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success, or appropriate exit code on failure.
  */
+
 int main(int argc, char *argv[])
 {
-    int source_fd, dest_fd;
-    ssize_t bytes_read, bytes_written;
-    char *buffer;
+	int source_fd, dest_fd;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
 
+	if (argc != 3)
+	print_usage_error();
 
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+	source_fd = open(argv[1], O_RDONLY);
+	if (source_fd == -1)
+	print_read_error(argv[1]);
 
-    buffer = create_buffer();
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+	S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (dest_fd == -1)
+	print_write_error(argv[2]);
 
-    source_fd = open(argv[1], O_RDONLY);
-    if (source_fd == -1)
-    {
-        perror("Error: Unable to open source file for reading");
-        exit(98);
-    }
+	while ((bytes_read = read(source_fd, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(dest_fd, buffer, bytes_read);
+	if (bytes_written == -1)
+		print_write_error(argv[2]);
+	}
 
-    dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (dest_fd == -1)
-    {
-        perror("Error: Unable to open or create destination file for writing");
-        close_file(source_fd);
-        exit(99);
-    }
+	if (bytes_read == -1)
+	print_read_error(argv[1]);
 
-     while ((bytes_read = read(source_fd, buffer, BUF_SIZE)) > 0)
-    {
-        bytes_written = write(dest_fd, buffer, bytes_read);
-        if (bytes_written == -1)
-        {
-            perror("Error: Unable to write to destination file");
-            close_file(source_fd);
-            close_file(dest_fd);
-            free(buffer);
-            exit(99);
-        }
-    }
+	if (close(source_fd) == -1)
+	print_close_error(source_fd);
 
-    if (bytes_read == -1)
-    {
-        perror("Error: Unable to read from source file");
-        close_file(source_fd);
-        close_file(dest_fd);
-        free(buffer);
-        exit(98);
-    }
+	if (close(dest_fd) == -1)
+	print_close_error(dest_fd);
 
-    free(buffer);
-    close_file(source_fd);
-    close_file(dest_fd);
-
-    return (0);
+	return (0);
 }
-
-
