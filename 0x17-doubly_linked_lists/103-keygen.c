318f7ccf9c33947e84193a585ddddfcb413f7c81
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-09 14:36:15 -0800

    errors

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Generate a key depending on a username for crackme5.
+ * @argc: Number of arguments passed.
+ * @argv: Arguments passed to main.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i, multiplier;
+	size_t len, sum;
+	char *char_set = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char key[7] = "      ";
+
+	if (argc != 2)
+	{
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
+	}
+
+	len = strlen(argv[1]);
+
+	key[0] = char_set[(len ^ 59) & 63];
+
+	for (i = 0, sum = 0; i < len; i++)
+		sum += argv[1][i];
+
+	key[1] = char_set[(sum ^ 79) & 63];
+
+	for (i = 0, multiplier = 1; i < len; i++)
+		multiplier *= argv[1][i];
+
+	key[2] = char_set[(multiplier ^ 85) & 63];
+
+	for (multiplier = argv[1][0], i = 0; i < len; i++)
+		if ((char)multiplier <= argv[1][i])
+			multiplier = argv[1][i];
+
+	srand(multiplier ^ 14);
+	key[3] = char_set[rand() & 63];
+
+	for (multiplier = 0, i = 0; i < len; i++)
+		multiplier += argv[1][i] * argv[1][i];
+
+	key[4] = char_set[(multiplier ^ 239) & 63];
+
+	for (multiplier = 0, i = 0; (char)i < argv[1][0]; i++)
+		multiplier = rand();
+
+	key[5] = char_set[(multiplier ^ 229) & 63];
+
+	printf("%s\n", key);
+	return (0);
+}
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,42 +11,69 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	if (*head == NULL)
 		return (-1);
+
 	if (index == 0)
+		return (delete_first_node(head));
+
+	return (delete_node_at_index(head, index));
+}
+
+/**
+ * delete_first_node - Deletes the first node of the list.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: 1 if the deletion succeeded, -1 if it failed.
+ */
+int delete_first_node(dlistint_t **head)
+{
+	if ((*head)->next != NULL)
 	{
-		if ((*head)->next != NULL)
-		{
-			(*head)->next->prev = NULL;
-			*head = (*head)->next;
-		}
-		else
-		{
-			free(*head);
-			*head = NULL;
-		}
-		return (1);
+		(*head)->next->prev = NULL;
+		*head = (*head)->next;
 	}
+	else
 	{
-		unsigned int i;
-		dlistint_t *current = *head;
-
-		for (i = 0; i < index && current != NULL; i++)
-		{
-			current = current->next;
-		}
-
-		if (current == NULL)
-			return (-1);
-
-		if (current->next != NULL)
-		{
-			current->next->prev = current->prev;
-		}
-
-		if (current->prev != NULL)
-		{
-			current->prev->next = current->next;
+		free(*head);
+		*head = NULL;
 	}
-	free(current);
+
 	return (1);
-	}
+}
+
+/**
+ * delete_node_at_index - Deletes a node at a specific index.
+ * @head: A pointer to the head of the list.
+ * @index: The index of the node to be deleted.
+ *
+ * Return: 1 if the deletion succeeded, -1 if it failed.
+ */
+int delete_node_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current = *head;
+
+	for (i = 0; i < index && current != NULL; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (-1);
+
+	delete_node(current);
+
+	return (1);
+}
+
+/**
+ * delete_node - Deletes a given node.
+ * @node: The node to be deleted.
+ */
+void delete_node(dlistint_t *node)
+{
+	if (node->next != NULL)
+		node->next->prev = node->prev;
+
+	if (node->prev != NULL)
+		node->prev->next = node->next;
+
+	free(node);
 }
