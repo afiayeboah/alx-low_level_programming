4fa69264c3470a569805df56f7d4e52147348ac5
afiayeboah <afia.yakuamoah@gmail.com>
2023-11-13 12:11:28 -0800

    jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/afiayeboah/
+wget -P .. https://raw.githubusercontent.com/afiayeboah/alx-low_level_programming/master/0x18-dynamic_libraries/libmakemewin.so
 export LD_PRELOAD="$PWD/../libmakemewin.so"
