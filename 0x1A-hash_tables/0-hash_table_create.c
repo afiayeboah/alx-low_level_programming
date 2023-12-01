1becb00f1cb8091c0f61206d00f21103374364cb
afiayeboah <afia.yakuamoah@gmail.com>
2023-12-01 05:30:59 -0800

    a

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int index;
+	hash_table_t *ht;
+	unsigned long int index;
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	return (NULL);
 
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-        return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+	free(ht);
+	return (NULL);
+	}
 
-    for (index = 0; index < size; index++)
-        ht->array[index] = NULL;
+	for (index = 0; index < size; index++)
+	ht->array[index] = NULL;
 
-    return (ht);
+	return (ht);
 }
