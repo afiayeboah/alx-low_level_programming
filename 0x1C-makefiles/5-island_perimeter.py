2388703ef6f8bcf364495d51ad7a109594a7eb69
afiayeboah <afia.yakuamoah@gmail.com>
2024-01-27 10:57:58 -0800

    6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,22 +3,25 @@
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid.
-    
+
     Args:
-    - grid (list of list of integers): Represents the island where 1 is land and 0 is water.
-    
+    - grid (list of list of integers): Represents the island
+
     Returns:
     - int: The perimeter of the island.
     """
-    perimeter = 0
-    
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    island_width = len(grid[0])
+    island_height = len(grid)
+    shared_edges = 0
+    island_size = 0
+
+    for i in range(island_height):
+        for j in range(island_width):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-                    
-    return perimeter
+                island_size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    shared_edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    shared_edges += 1
+
+    return island_size * 4 - shared_edges * 2
